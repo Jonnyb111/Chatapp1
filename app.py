85afd11d61207f78e4d79546883652ab28f584f2
jonnyb111 <jonjustiniano@gmail.com>
2023-10-15 23:58:18 -0400

    Update to readme's and instructions+future plans

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 import streamlit as st
 import openai
+import requests
 
 # Read OpenAI API key from needful.txt
 with open("needful.txt") as f:
@@ -10,6 +11,10 @@ openai_url = "https://api.openai.com/v1/completions"
 openai_model = "text-davinci-002"
 openai_prompt = "Hello, how are you?"
 
+# Define Langchain api and params
+langchain_url = "https://api.openai.com/v1/completions"
+langchain_key = openai_key
+
 # Define Streamlit app
 def app():
     st.set_page_config(page_title="OpenAI GPT-3 Chatbot", page_icon=":robot_face:")
@@ -17,16 +22,30 @@ def app():
     st.write("This is a test of the emergency broadcast chat system")
     user_input = st.text_input("Type your message below...")
     if user_input:
+        # Preprocess user input using LangChain API
+        langchain_data = {
+            "text": user_input,
+            "model": "gpt-3.5",
+            "api_key": langchain_key,
+        }
+        langchain_response = requests.post(langchain_url, json=langchain_data)
+        langchain_output = langchain_response.json()
+        langchain_text = langchain_output["text"]
         # Generate response using OpenAI API
-        response = openai.Completion.create(
-            engine=openai_model,
-            prompt=openai_prompt + " " + user_input,
-            max_tokens=1024,
-            n=1,
-            stop=None,
-            temperature=0.5,
-        )
-        message = response.choices[0].text.strip()
+        openai_data = {
+            "prompt": openai_prompt + " " + langchain_text,
+            "max_tokens": 1024,
+            "n": 1,
+            "stop": None,
+            "temperature": 0.5,
+        }
+        openai_headers = {
+            "Content-Type": "application/json",
+            "Authorization": f"Bearer {openai_key}",
+        }
+        openai_response = requests.post(openai_url, json=openai_data, headers=openai_headers)
+        openai_output = openai_response.json()
+        message = openai_output["choices"][0]["text"].strip()
         st.write("ChatBot Reply:  " + message)
 
 # Run Streamlit app
--- readme.md
+++ readme.md
@@ -1,10 +1,12 @@
 # Chat bot configuration soon to add docker details once i figure out a few more things.
-
 # Configure
-
-Create a `.env` file from the environment template file `env.example`
+# you will need to create a env, install the requirements and then you can launch it 
+'streamlit run app.py'
 
 # Applications will be made for a future bot
-## App 1 - Support Agent Bot
+## App 1 - chatbot, uses local host to interact with the chat via web browser (streamlit).
 
 UI: http://localhost:8501
+
+#Future: Docker file template to container the env to an image. 
+#Currently need to remove my api keys and process the env details in a different way in order to allow for apikey changes dynamically by updating the file.
